de4bc87377ba2fe7d21d273f60c3f29dce68b8ca
Slim-Beatnik <totem64@gmail.com>
2025-02-21 12:06:48 -0700

    Finished navbar

--- ct-Module05-bootstrap-Assignment02-recreatePage/assets/script/gamespotScripts.js
+++ ct-Module05-bootstrap-Assignment02-recreatePage/assets/script/gamespotScripts.js
@@ -10,6 +10,15 @@ function swapVisById(elId) {
     els.forEach((el) => el.hidden = !el.hidden);
 }
 
+function toggleNavIcons() {
+    const openIcon = document.getElementById("navToggleOpen");
+    const closeIcon = document.getElementById("navToggleClose");
+
+    // Toggle visibility of the icons
+    openIcon.classList.toggle("d-none");
+    closeIcon.classList.toggle("d-none");
+}
+
 // copy height from absolute div and apply it to spacer div
 function setSpacerHeight(spacerId, idToMatchHeight) {
     const spacer = document.getElementById(spacerId);
@@ -17,3 +26,25 @@ function setSpacerHeight(spacerId, idToMatchHeight) {
     spacer.style.height = heightToMatch + 'px';
 }
 
+// adjust gamespot logo based on window size
+function logoSizeUpdate() {
+    const logo = document.getElementById('gsLogo');
+    let windowWidth = window.innerWidth;
+    if (windowWidth <= 768) {
+        logo.style.width = '30px';
+        logo.style.height = '30px';
+    } else {
+        logo.style.width = '40px';
+        logo.style.height = '40px';
+    }
+}
+
+function swapMainContent(url) {
+    // fetch and get response (works like ready as long as files exist)
+    fetch(url)
+        .then(response => response.text())
+        .then(fetchedUrlContent => {
+            // swap new page into the content div
+            document.getElementById('mainContent').innerHTML = fetchedUrlContent;
+    });
+}
